dd6578280a8a04933188ae063359f8a866bc37a7
andryusdev <andryusdev@gmail.com>
2025-11-01 16:04:02 -0400

    desarrollo del diseño del login

--- italian_cuisine_app/urls.py
+++ italian_cuisine_app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('panel/', views.panel_principal, name='panel_principal'),
 ]
