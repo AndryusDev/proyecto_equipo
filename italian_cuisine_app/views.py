4b3afdee75a914a3ad1594054e5aea7742069930
josue <firej390@gmail.con>
2025-11-01 17:01:32 -0400

    lista de pedidos(no se si nesesita los botones)

--- italian_cuisine_app/views.py
+++ italian_cuisine_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Pedido
 
 # Create your views here.
+
+def lista_pedidos(request):
+    # El modelo Pedido puede definir `fecha` o `hora_creacion` como campo DateTimeField.
+    # Elegimos el que exista para ordenar.
+    if any(field.name == 'hora_creacion' for field in Pedido._meta.fields):
+        pedidos = Pedido.objects.all().order_by('-hora_creacion')
+    else:
+        pedidos = Pedido.objects.all().order_by('-fecha')
+
+    return render(request, 'pedidos/lista_pedidos.html', {'pedidos': pedidos})
